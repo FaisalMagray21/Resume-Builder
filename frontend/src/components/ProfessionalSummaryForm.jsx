864094667aee3ac8f7daeb9e5bcc15b9f7217805
FaisalMagray21 <136814501+FaisalMagray21@users.noreply.github.com>
2025-10-14 08:16:13 -0700

    add a colorpicker,template and resumedisplay

---
+++ frontend/src/components/ProfessionalSummaryForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProfessionalSummaryForm = ({ data, onChange }) => {
+  return (
+    <div>ProfessionalSummaryForm</div>
+  )
+}
+
+export default ProfessionalSummaryForm
--- frontend/src/pages/ResumeBuilder.jsx
+++ frontend/src/pages/ResumeBuilder.jsx
@@ -13,6 +13,9 @@ import {
   User,
 } from 'lucide-react'
 import PersonalInfoForm from '../components/PersonalInfoForm'
+import ResumePreview from '../components/ResumePreview'
+import TemplateSelector from '../components/TemplateSelector'
+import ColorPicker from '../components/ColorPicker'
 
 const ResumeBuilder = () => {
   const { resumeId } = useParams()
@@ -86,7 +89,12 @@ const ResumeBuilder = () => {
 
               {/* section navigation */}
               <div className="flex justify-between items-center mb-6 border-b border-gray-300 py-1">
-                <div></div>
+                <div className='flex items-center gap-2'>
+
+                  <TemplateSelector selectedTemplate={resumeData.template}
+                  onChange={(template)=>{setResumeData(prev=>({...prev,template}))}}/>
+                  <ColorPicker selectedColor={resumeData.accent_color} onChange={(color)=>{setResumeData(prev=>({...prev,accent_color:color}))}}/>
+                </div>
                 <div className="flex items-center">
                   {activeSectionIndex !== 0 && (
                     <button
@@ -140,7 +148,13 @@ const ResumeBuilder = () => {
           </div>
 
           {/* Right panel - Resume Preview */}
-          <div>{/* Empty for now */}</div>
+          <div className='lg:col-span-7 max-lg:mt-6'>
+            <div>
+              {/* ----button----  */}
+
+            </div>
+          <ResumePreview data={resumeData} template={resumeData.template} accentColor={resumeData.accent_color} />
+          </div>
         </div>
       </div>
     </div>
