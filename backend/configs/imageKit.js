12a78456c79ef28a27db2ebe17ccb4b60d60b607
FaisalMagray21 <fmagray287@gmail.com>
2026-07-21 18:28:33 +0500

    solve imagekit

--- backend/configs/imageKit.js
+++ backend/configs/imageKit.js
@@ -2,7 +2,7 @@ import ImageKit from "@imagekit/nodejs";
 
 const imagekit = new ImageKit({
  // publicKey: process.env.IMAGEKIT_PUBLICKEY,      // ✅ required
-  privateKey: process.env.IMAGEKIT_PRIVATEKEY,    // ✅ required
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,    // ✅ required
   // urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,  // ✅ required
 });
 
